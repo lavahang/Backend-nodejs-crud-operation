fcab892c1d350088ecb21f249c200734992d3d3f
Lavahang Phago <135295990+lavahang@users.noreply.github.com>
2024-11-27 21:21:13 +0545

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-require('dotenv').config();
+// require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
